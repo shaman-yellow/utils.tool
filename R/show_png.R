a3455e9da30e3676ca18dc1307ecc2cb35cba425
echo <echo@pop-os.localdomain>
2022-06-26 15:18:38 +0800

    show.png

---
+++ R/show_png.R
@@ -0,0 +1,19 @@
+show_png <- 
+  function(
+           file,
+           size = "500x"
+           ){
+    if(grepl("\\.svg$", file)){
+      tofile <- sub("\\.svg$", ".png", file)
+      system(
+        paste(
+          "cairosvg -i", file, "-d 300", "-o", tofile
+        )
+      )
+      file <- tofile
+    }
+    png <- magick::image_read(file)
+    png <- magick::image_resize(png, size)
+    grid::grid.raster(png)
+  }
+
