4d5a5425bb32d175ec8f4756b1ebc6659ba3cc43
echo <shaman.yellow@foxmail.com>
2023-08-05 06:50:01 +0800

    Sat Aug  5 06:50:01 AM CST 2023

--- R/set_export.no.R
+++
@@ -1,9 +0,0 @@
-# set_export.no <- function(df, col = "name"){
-    # tmp <- data.table::data.table(name = unique(df[[col]])) %>% 
-    #   dplyr::mutate(., No = 1:nrow(.)) %>% 
-    #   dplyr::select(No, name)
-    # df <- merge(df, tmp, by.x = col, by.y = "name", all.x = T, sort = F) %>% 
-    #   dplyr::relocate(No) %>% 
-    #   dplyr::as_tibble()
-    # return(df)
-  # }
