a68ab4d6e90f8f6403122032eb844bc852eaa48a
echo <echo@pop-os.localdomain>
2023-09-04 20:59:09 +0800

    Mon Sep  4 08:59:09 PM CST 2023

---
+++ R/remote_api.R
@@ -0,0 +1,101 @@
+# ==========================================================================
+# for remote files operation
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+rem_list.files <- function(path, pattern, ...) {
+  if (!check_remote()) {
+    if (length(path) > 1) {
+      sapply(path, list.files, pattern = pattern, simplify = F)
+    } else {
+      list.files(path, pattern, ...)
+    }
+  } else {
+    list.remote(path, pattern,
+      remote = x@params$remote, wd = x@params$wd
+    )
+  }
+}
+
+rem_file.copy <- function(...) {
+  if (!check_remote()) {
+    file.copy(...)
+  } else {
+    stop("...")
+  }
+}
+
+rem_file.rename <- function(...) {
+  if (!check_remote()) {
+    file.rename(...)
+  } else {
+    stop("...")
+  }
+}
+
+rem_file.remove <- function(...) {
+  if (!check_remote()) {
+    file.remove(...)
+  } else {
+    stop("...")
+  }
+}
+
+rem_file.exists <- function(...) {
+  if (!check_remote()) {
+    file.exists(...)
+  } else {
+    stop("...")
+  }
+}
+
+rem_normalizePath <- function(...) {
+  if (!check_remote()) {
+    normalizePath(...)
+  } else {
+    stop("...")
+  }
+}
+
+rem_unlink <- function(...) {
+  if (!check_remote()) {
+    unlink(...)
+  } else {
+    stop("...")
+  }
+}
+
+rem_list.files <- function(...) {
+  if (!check_remote()) {
+    list.files(...)
+  } else {
+    stop("...")
+  }
+}
+
+rem_dir.create <- function(...) {
+  if (!check_remote()) {
+    dir.create(...)
+  } else {
+    stop("...")
+  }
+}
+
+check_remote <- function(n = 1) {
+  remote <- F
+  if (exists("x", envir = parent.frame(n))) {
+    x <- get("x", envir = parent.frame(n))
+    if (is(x, "job")) {
+      if (is.remote(x)) {
+        remote <- T
+      }
+    }
+  }
+  remote
+}
+
+set_if_null <- function(object, value) {
+  if (is.null(object)) 
+    value
+  else
+    object
+}
